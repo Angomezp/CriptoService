923dd4fcbe03de9e0f3fdea929f740682d6a1029
Angomezp <angeldavidgomez06@gmail.com>
2026-05-28 13:32:22 -0500

    Fix: authcontroller reveals too much info
    Tested 1 time, works

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,30 +4,30 @@ import AuthService  from '../services/auth.service.js';
 const authService = new AuthService();
 
 export const register = async (req: Request, res: Response) => {
-    if (!req.body.nombre || !req.body.password || !req.body.correo) {
+    const body = req.body;
+    if (!body || !body.nombre || !body.password || !body.correo) {
         return res.status(400).json({ message: 'Datos incompletos' });
     }
     try {
-        const validRegister = await authService.register(req.body.nombre, req.body.correo, req.body.password);
+        const validRegister = await authService.register(body.nombre, body.correo, body.password);
         if (!validRegister) {
             return res.status(400).json({ message: 'Registro fallido' });
         }
         res.status(201).json({ message: 'Usuario registrado exitosamente' });
-    } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : String(error);
-        res.status(500).json({ message: 'Error del servidor al registrar usuario', error: errorMessage });
+    } catch (error: any) {
+        res.status(500).json({ message: 'Error del servidor al registrar usuario', error });
     }
 };
 
 export const login = async (req: Request, res: Response) => {
-    if (!req.body.nombre || !req.body.password) {
+    const body = req.body;
+    if (!body || !body.nombre || !body.password) {
         return res.status(400).json({ message: 'Datos incompletos' });
     }
     try {
-        const user = await authService.login(req.body.nombre, req.body.password);
+        const user = await authService.login(body.nombre, body.password);
         res.status(200).json({ message: 'Login successful', user });
-    } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : String(error);
-        res.status(401).json({ message: 'Credenciales inválidas', error: errorMessage });
+    } catch (error: any) {
+        res.status(401).json({ message: 'Credenciales inválidas', error });
     }
 };
