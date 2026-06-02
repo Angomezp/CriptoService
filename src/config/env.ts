6d878bbdfca01c965bbafbc87a6a51815da9b942
Jhon Part <jhprieto@unal.edu.co>
2026-06-01 19:20:58 -0500

    db montada en aws con postgres - entitie agregada para trabajar ETL con Glue

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,36 +1,10 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
-dotenv.config();
-
-interface EnvConfig {
-    serverPort: number;
-    dbHost: string;
-    dbPort: number;
-    dbUser: string;
-    dbPassword: string;
-    dbName: string;
-    encryptionKey: string; 
-    dbType: string;
-}
-
-function validateEnv() {
-    const requiredEnvVars = ['SV_PORT', 'DB_HOST', 'DB_PORT', 'DB_USER', 'DB_PASSWORD', 'DB_NAME', 'ENCRYPTION_KEY', 'DB_TYPE'];
-    const missingEnvVars = requiredEnvVars.filter((varName) => !process.env[varName]);
-
-    if (missingEnvVars.length > 0) {
-        throw new Error(`Missing required environment variables: ${missingEnvVars.join(', ')}`);
-    }
-}
-
-validateEnv();
-
-export const env: EnvConfig = {
-    serverPort: Number(process.env.SV_PORT),
-    dbHost: process.env.DB_HOST as string, 
-    dbPort: Number(process.env.DB_PORT),
-    dbUser: process.env.DB_USER as string,
-    dbPassword: process.env.DB_PASSWORD as string,
-    dbName: process.env.DB_NAME as string,
-    encryptionKey: process.env.ENCRYPTION_KEY as string,
-    dbType: process.env.DB_TYPE as string
+export const env = {
+  dbHost: process.env.DB_HOST,
+  dbPort: Number(process.env.DB_PORT),
+  dbUsername: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  serverPort: Number(process.env.PORT),
 };
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,27 @@
 import cors from 'cors';
 import express from 'express';
+import 'reflect-metadata';
+import { AppDataSource } from './config/datasource.js';
 import { env } from './config/env.js';
 import router from './config/router.js';
-import "reflect-metadata";
-import { AppDataSource } from './config/datasource.js';
 
+async function main() {
+  try {
+    await AppDataSource.initialize();
+    console.log('Database connected');
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Database connected");
-  })
-  .catch((error) => {
-    console.error("DB connection error:", error);
-  });
+    const app = express();
 
-const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use('/api', router);
 
-app.use(cors());
-app.use(express.json());
-app.use('/api', router);
+    app.listen(env.serverPort, () => {
+      console.log(`Server running on port ${env.serverPort}`);
+    });
+  } catch (error) {
+    console.error('Error initializing application:', error);
+  }
+}
 
-app.listen(env.serverPort, () => {
-	console.log(`Server running on port ${env.serverPort}`);
-});
+main();
